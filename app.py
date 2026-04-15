799c8b23d44ae84460d65c6f842f8eeba31f6064
Sophia Verardo <101429997+sweetsoph@users.noreply.github.com>
2026-04-15 19:35:10 -0300

    importando supabase e configurando
    Added environment variable loading and Supabase client initialization.

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
+from supabase import create_client, Client
+
+load_dotenv()
+
+url = os.getenv('SUPABASE_URL')
+key = os.getenv('SUPABASE_KEY')
+
+supabase: Client = create_client(url, key)
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,4 +17,4 @@ def hello():
 
 if __name__ == "__main__":
     # o host deve ser '0.0.0.0' para que o app seja acessível fora do container
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
